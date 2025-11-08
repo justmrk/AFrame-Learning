80c24cf003c74ffe29d0b9228b934871f7ee8e08
justmrk <119432905+justmrk@users.noreply.github.com>
2025-11-08 21:14:11 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
  document.querySelector('#myBox').addEventListener('click', e => {
     e.target.setAttribute('color', '#EF2D5E');
   });
+
+ 
+  AFRAME.registerComponent('click-change', {
+    init: function () {
+      let el = this.el;
+      el.addEventListener('click', function () {
+        el.setAttribute('color', '#39FF14'); // سبز فسفری!
+        el.setAttribute('position', {x: -1.5 , y: 2, z: -3});
+      });
+    }
+  });
+
